53548569a9a0a084042b1ca4fcefff328fc1ef04
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-10-18 11:45:33 +0530

    initial commit

---
+++ express middleware/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+function middleware1(req, res, next) {
+  console.log("milldeware 1");
+  next();
+}
+
+function middlewar2(req, res, next) {
+  console.log("milldeware 2");
+  next(new Error("error"));
+}
+
+function errorHandler(err, req, res, next) {
+  res.send("<h1>error</h1>");
+  next();
+}
+
+
+
+app.use(middleware1);
+
+app.get("/", middlewar2, (req, res) => {
+  res.send(`<h1>hello world</h1>`);
+});
+app.use(errorHandler);
+
+app.listen(8081, () => console.log("listnint on http://localhost:8081"));
